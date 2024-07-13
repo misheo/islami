9a1fc5a050f12b2c823b531451da5d60a1c83d88
Said Nassar <saidnassar18@gmail.com>
2024-07-13 18:52:39 +0200

    Refactor Quran screen UI and add localization support
    - Refactored the UI of the Quran screen to display a list of suras with their names and number of verses.
    - Added localization support to the DefaultBackground widget and the Quran screen.
    - Updated the AppBar title of the DefaultBackground widget to use the app title from the localization file.
    - Updated the text styles of the Quran screen to use the theme's large body and title styles.
    - Added a separator between each sura in the list.
    - Updated the layout of the sura list to use an Expanded ListView.
    - Updated the code formatting to follow best practices.

--- lib/core/theme/widgets/default_background.dart
+++ lib/core/theme/widgets/default_background.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class DefaultBackground extends StatelessWidget {
   const DefaultBackground({super.key, required this.child});
@@ -6,12 +7,18 @@ class DefaultBackground extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Image.asset('assets/images/light_bg.png' , width:  double.infinity, fit: BoxFit.fill, height:  double.infinity,),
-        child
-
-      ]
-    );
+    return Stack(children: [
+      Image.asset(
+        'assets/images/light_bg.png',
+        width: double.infinity,
+        fit: BoxFit.fill,
+        height: double.infinity,
+      ),
+      Scaffold(
+          appBar: AppBar(
+            title: Text(AppLocalizations.of(context)!.app_title),
+          ),
+          body: child)
+    ]);
   }
 }
