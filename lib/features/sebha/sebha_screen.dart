de745775968ade5ea9f4290b2a35dc6f995c6cc2
Said Nassar <saidnassar18@gmail.com>
2024-07-15 22:45:23 +0200

    Add dark mode support to Sebha screen and use SettingsProvider
    Added support for dark mode in the Sebha screen by using the SettingsProvider to determine the current theme. The Sebha screen now displays a different image and color scheme based on the current theme.

--- lib/features/sebha/sebha_screen.dart
+++ lib/features/sebha/sebha_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 import '../../core/theme/my_colors.dart';
+import '../../providers/settings_provider.dart';
 
 class SebhaScreen extends StatefulWidget {
   const SebhaScreen({super.key});
@@ -15,10 +17,12 @@ class _SebhaScreenState extends State<SebhaScreen> {
   int count = 0;
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<SettingsProvider>(context);
     return Column(
       children: [
-  
-        Image.asset('assets/images/sebha.png'),
+        provider.isLightMode
+            ? Image.asset('assets/images/sebha.png')
+            : Image.asset('assets/images/dark_sibha.png'),
         SizedBox(
           height: 57.h,
         ),
@@ -36,13 +40,17 @@ class _SebhaScreenState extends State<SebhaScreen> {
               decoration: BoxDecoration(
                 shape: BoxShape.rectangle,
                 borderRadius: BorderRadius.circular(25),
-              color: MyColors.primaryLightColor,
-              
+                color: provider.isLightMode
+                    ? MyColors.primaryLightColor
+                    : MyColors.primaryDarkColor,
               ),
-              child:  Center(child: Text('$count',)),
+              child: Center(
+                  child: Text(
+                '$count',
+              )),
             ),
           ],
-        )  , 
+        ),
         SizedBox(
           height: 22.h,
         ),
@@ -55,13 +63,17 @@ class _SebhaScreenState extends State<SebhaScreen> {
               decoration: BoxDecoration(
                 shape: BoxShape.rectangle,
                 borderRadius: BorderRadius.circular(25),
-              color: MyColors.primaryLightColor,
-              
+                color: provider.isLightMode
+                    ? MyColors.primaryLightColor
+                    : MyColors.goldDarkColor,
               ),
-              child:  Center(child: Text('$count',)),
+              child: Center(
+                  child: Text(
+                '$count',
+              )),
             ),
           ],
-        ) 
+        )
       ],
     );
   }
